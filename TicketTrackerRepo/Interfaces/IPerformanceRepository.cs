a19d47c0952d8c4f2d5c6a3d8cd771f444043817
Brian Randall <brianerandall@gmail.com>
2017-07-31 20:10:07 -0400

    Delete IPerformanceRepository.cs

--- TicketTrackerRepo/Interfaces/IPerformanceRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using TicketTrackerEntityModel;
-using TicketTrackerRepo.DTOs;
-
-namespace TicketTrackerRepo.Interfaces
-{
-    public interface IPerformanceRepository : ITicketTrackerRepo<Performance, PerformanceDto>
-    {
-    }
-}
